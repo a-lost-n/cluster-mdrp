88afdac1359b97e663ff3d9e93d52cd172313b16
a-lost-n <freewer@alostnPC.localdomain>
2023-07-04 11:53:32 -0500

    transf v1.04

--- train.sh
+++ train.sh
@@ -5,7 +5,10 @@
 #SBATCH --tasks-per-node=1             # Number of tasks (processes) per node
 #SBATCH --cpus-per-task=8              # Number of CPUs per task
 
-module unload python/2.7.17 # carga el modulo de python version 2.7.17
-module load python/3.9.2 
+module purge
+module load miniconda/3.0
+conda activate tesis
+# module load python/3.9.2 
 python train.py # siendo prueba_python.py el nombre del programa python
-module unload python/3.9.2
+# module unload python/3.9.2
+conda deactivate tesis
