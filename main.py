8553e4bf590cedc0e728a4d550ca4e55e8198f1b
a-lost-n <freewer@alostnPC.localdomain>
2023-07-04 12:52:26 -0500

    transf v1.08

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from src import *
 
-if __name__ == '__main__':
-    agent = DDQNAgent(restaurant_array=[2,6,2], grid_size=100, randseed=25, filename="model")
+agent = DDQNAgent(restaurant_array=[2,6,2], grid_size=100, randseed=25, filename="model")
 
-    episodios_terminados = 15
-    agent.train(episodes=5, batch_size=16, epsilon=0.99**episodios_terminados, epsilon_decay=0.99)
+episodios_terminados = 15
+agent.train(episodes=5, batch_size=16, epsilon=0.99**episodios_terminados, epsilon_decay=0.99)
 
-    agent.save("model")
+agent.save("model")
